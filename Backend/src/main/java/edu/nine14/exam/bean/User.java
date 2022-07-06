c68427d886a4729939f422dcb8c64f74f96b9298
Hell-Tractor <1104840448@qq.com>
2022-07-06 20:23:50 +0800

    Modified API result and finished sql connection
    	modified:   Backend/pom.xml
    	modified:   Backend/src/main/java/edu/nine14/common/ApiResult.java
    	modified:   Backend/src/main/java/edu/nine14/common/HttpCode.java
    	new file:   Backend/src/main/java/edu/nine14/exam/bean/User.java
    	new file:   Backend/src/main/java/edu/nine14/exam/controller/Login.java
    	new file:   Backend/src/main/java/edu/nine14/exam/dao/UserRepository.java
    	deleted:    Backend/src/main/resources/application.properties
    	new file:   Backend/src/main/resources/application.yaml
    	modified:   Backend/src/test/java/edu/nine14/exam/OnlineExamApplicationTests.java
    	modified:   sql/model/datatypes/Diagrams.local
    	new file:   sql/model/dmd_open.local
    	modified:   sql/model/logical/Objects.local
    	modified:   sql/model/rel/7DC7B082-186BEB290AAB/Objects.local

---
+++ Backend/src/main/java/edu/nine14/exam/bean/User.java
@@ -0,0 +1,52 @@
+package edu.nine14.exam.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password", columnDefinition = "char")
+    private String password;
+    @Column(name = "user_type", columnDefinition = "char")
+    private String userType;
+    @Column(name = "auth_id", columnDefinition = "char")
+    private String authId;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getAuthId() {
+        return authId;
+    }
+
+    public void setAuthId(String authId) {
+        this.authId = authId;
+    }
+}
--- Backend/src/test/java/edu/nine14/exam/OnlineExamApplicationTests.java
+++ Backend/src/test/java/edu/nine14/exam/OnlineExamApplicationTests.java
@@ -1,13 +1,27 @@
 package edu.nine14.exam;
 
+import edu.nine14.exam.bean.User;
+import edu.nine14.exam.dao.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Optional;
 
 @SpringBootTest
 class OnlineExamApplicationTests {
+	@Autowired
+	UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void TestUserRepo() {
+		Optional<User> user = userRepository.findById("admin");
+		System.out.println(user.get().getUserType());
 	}
 
 }

e87b358e5c474aed8cf778748267476cdb769d9e
吴佳熙 <1006961384@qq.com>
2022-07-13 20:21:15 +0800

    update

---
+++ Backend/src/main/java/edu/nine14/exam/service/ProfessionService.java
@@ -0,0 +1,21 @@
+package edu.nine14.exam.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import edu.nine14.exam.entity.Profession;
+import edu.nine14.exam.dao.ProfessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.security.auth.login.FailedLoginException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class ProfessionService {
+    @Autowired
+    private ProfessionRepository professionRepository;
+}

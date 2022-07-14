d97dac1751adc491fa6f8ca40ab210de34015bb1
吴佳熙 <1006961384@qq.com>
2022-07-14 16:59:04 +0800

    新增根据专业和特定的两个专业方向查询题目的方法

--- Backend/src/main/java/edu/nine14/exam/dao/QuestionDirectionRepository.java
+++ Backend/src/main/java/edu/nine14/exam/dao/QuestionDirectionRepository.java
@@ -1,10 +1,17 @@
 package edu.nine14.exam.dao;
 
-import edu.nine14.exam.entity.Question;
+import edu.nine14.exam.entity.QuestionDirection;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
-@Repository
-public interface QuestionDirectionRepository extends CrudRepository<Question, Integer> {
+import java.util.List;
 
+@Repository
+public interface QuestionDirectionRepository extends CrudRepository<QuestionDirection, Integer> {
+    @Query("select q.questionID,q.body,q.answer,q.selection from QuestionDirection q where q.direction.profession.professionID=?1 and q.type=?2")
+    List<Object> questionForProfession(Integer directionID,String type);
+
+    @Query("select q.questionID,q.body,q.answer,q.selection from QuestionDirection q where q.direction.profession.professionID=?1 and q.type=?2 and q.direction.directionName IN (?3,?4)")
+    List<Object> questionForDirection(Integer directionID,String type,String direction1,String direction2);
 }

aa40d1bea59b95a03b1906ccb7bd80d17c0e8e1d
吴佳熙 <1006961384@qq.com>
2022-07-14 16:35:41 +0800

    新增根据专业名称查询专业id的方法

--- Backend/src/main/java/edu/nine14/exam/dao/ProfessionRepository.java
+++ Backend/src/main/java/edu/nine14/exam/dao/ProfessionRepository.java
@@ -3,6 +3,7 @@ package edu.nine14.exam.dao;
 import edu.nine14.exam.entity.Profession;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,6 +11,6 @@ import java.util.List;
 
 @Repository
 public interface ProfessionRepository extends CrudRepository<Profession, Integer> {
-    @Query("select id from profession where name= professionName")
-    Integer findByProfessionName(String professionName);
+    @Query("select p.professionID from Profession p where p.professionName=?1")
+    Object findByProfessionName(String professionName);
 }

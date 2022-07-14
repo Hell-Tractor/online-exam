0a2752ffc3d7a2acd455c459fd79cc373a66a06b
吴佳熙 <1006961384@qq.com>
2022-07-14 11:21:58 +0800

    update

--- Backend/src/main/java/edu/nine14/exam/dao/ProfessionRepository.java
+++ Backend/src/main/java/edu/nine14/exam/dao/ProfessionRepository.java
@@ -2,10 +2,14 @@ package edu.nine14.exam.dao;
 
 import edu.nine14.exam.entity.Profession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 @Repository
 public interface ProfessionRepository extends CrudRepository<Profession, Integer> {
-
+    @Query("select id from profession where name= professionName")
+    Integer findByProfessionName(String professionName);
 }
--- Backend/src/main/java/edu/nine14/exam/dao/QuestionDirectionRepository.java
+++ Backend/src/main/java/edu/nine14/exam/dao/QuestionDirectionRepository.java
@@ -1,11 +1,10 @@
 package edu.nine14.exam.dao;
 
 import edu.nine14.exam.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface QuestionRepository extends CrudRepository<Question, Integer> {
+public interface QuestionDirectionRepository extends CrudRepository<Question, Integer> {
 
 }

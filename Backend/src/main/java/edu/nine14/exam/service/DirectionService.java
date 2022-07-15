553a273a6fe3633280d5baacb5ec6d3ea3ed58ce
ZhichengLin <1317864235@qq.com>
2022-07-15 22:36:48 +0800

    增加一些方法

--- Backend/src/main/java/edu/nine14/exam/service/DirectionService.java
+++ Backend/src/main/java/edu/nine14/exam/service/DirectionService.java
@@ -2,8 +2,11 @@ package edu.nine14.exam.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import edu.nine14.exam.dao.DirectionSingleRepository;
 import edu.nine14.exam.entity.Direction;
 import edu.nine14.exam.dao.DirectionRepository;
+import edu.nine14.exam.entity.DirectionSingle;
+import edu.nine14.exam.entity.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,4 +21,34 @@ import java.util.Optional;
 public class DirectionService {
     @Autowired
     private DirectionRepository directionRepository;
+
+    @Autowired
+    private DirectionSingleRepository directionSingleRepository;
+
+    public Object getAllDirection(){
+        return directionRepository.findAll();
+    }
+
+    public void editDirection(Direction direction){
+        try {
+            directionSingleRepository.editDirection(direction.getDirectionName(),direction.getProfession().getProfessionID()
+                    ,direction.getDirectionID());
+        }catch (Exception e){
+            throw e;
+        }
+    }
+
+    public Object selectById(Integer directionID){
+        Optional<DirectionSingle> direction = directionSingleRepository.findById(directionID);
+        if (direction.isEmpty())
+            throw new IllegalArgumentException("Question not found");
+        return direction.get();
+    }
+
+    public void deleteQuestion(Integer directionID){
+        Optional<DirectionSingle> question = directionSingleRepository.findById(directionID);
+        if (question.isEmpty())
+            throw new IllegalArgumentException("Question not found");
+        directionSingleRepository.delete(question.get());
+    }
 }
--- Backend/src/main/java/edu/nine14/exam/service/QuestionDirectionService.java
+++ Backend/src/main/java/edu/nine14/exam/service/QuestionDirectionService.java
@@ -1,8 +1,18 @@
 package edu.nine14.exam.service;
 
 import edu.nine14.exam.dao.QuestionDirectionRepository;
+import edu.nine14.exam.entity.Question;
+import edu.nine14.exam.entity.QuestionDirection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionDirectionService {
@@ -13,6 +23,13 @@ public class QuestionDirectionService {
         return questionDirectionRepository.findAll();
     }
 
+    public Object selectById(Integer questionDirectionID){
+        Optional<QuestionDirection> questionDirection = questionDirectionRepository.findById(questionDirectionID);
+        if (questionDirection.isEmpty())
+            throw new IllegalArgumentException("Question not found");
+        return questionDirection.get();
+    }
+
     public Object questionForProfession(Integer professionID,String type){
         return questionDirectionRepository.questionForProfession(professionID,type);
     }
@@ -20,4 +37,36 @@ public class QuestionDirectionService {
     public Object questionForDirection(Integer directionID,String type,String direction1,String direction2){
         return questionDirectionRepository.questionForDirection(directionID,type,direction1,direction2);
     }
+
+    /**
+     * 按照所需要的条件进行查找
+     * @param page 页数，默认第0页
+     * @param size 每页显示的条数
+     * @param direction 专业方向
+     * @param id 问题id
+     * @param profession 专业
+     * @param type 题目类型
+     * @return 返回符合条件的所有题目
+     */
+    public Page<QuestionDirection> findByCondition(Integer page, Integer size, Integer direction,
+                                                   Integer profession,Integer id, String type){
+        Pageable pageable = PageRequest.of(page, size);
+        return questionDirectionRepository.findAll((root, criteriaQuery, criteriaBuilder)->{
+            List<Predicate> predicates = new ArrayList<Predicate>();
+            if(direction!=null){
+                predicates.add(criteriaBuilder.equal(root.get("direction").get("directionID"),direction));
+            }
+            if(id!=null){
+                predicates.add(criteriaBuilder.equal(root.get("questionID"),id));
+            }
+            if(profession!=null){
+                predicates.add(criteriaBuilder.equal(root.get("direction").get("profession").get("professionID"),profession));
+            }
+            if(type!=null){
+                predicates.add(criteriaBuilder.equal(root.get("type"),type));
+            }
+            return criteriaQuery.where(predicates.toArray(new Predicate[predicates.size()])).getRestriction();
+        },pageable);
+    }
+
 }

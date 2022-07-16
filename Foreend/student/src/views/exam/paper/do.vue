7fc277a58732de7cb2bd240b483920bd1c7cdd26
ryanregal <1822327087@qq.com>
2022-07-16 22:21:20 +0800

    前端试卷生成部分，没对后端API

--- Foreend/student/src/views/exam/paper/do.vue
+++ Foreend/student/src/views/exam/paper/do.vue
@@ -9,15 +9,26 @@
 
 <!--      考试部分-->
       <el-main>
-        <el-form :model="form" ref="form" v-loading="formLoading" label-width="100px">
+        <el-form :model="form" ref="form" v-loading="formLoading" label-width="200px" >
           <el-row :key="index"  v-for="(titleItem,index) in form.titleItems">
             <h3>{{titleItem.name}}</h3>
             <el-card class="exampaper-item-box" v-if="titleItem.questionItems.length!==0">
-              <el-form-item :key="questionItem.itemOrder" :label="questionItem.itemOrder+'.'"
+<!--                题目-->
+              <el-form-item :key="questionItem.itemOrder" :label="questionItem.itemOrder+'.'+questionItem.title"
                             v-for="questionItem in titleItem.questionItems"
-                            class="exam-question-item" label-width="50px" :id="'question-'+ questionItem.itemOrder">
-                <QuestionEdit :qType="questionItem.type" :question="questionItem"
-                              :answer="answer.answerItems[questionItem.itemOrder-1]"/>
+                            class="exam-question-item" label-width="auto" label-position="left"  :id="'question-'+ questionItem.itemOrder">
+                <el-form-item :key="questionItem.itemOrder"
+                              class="exam-question-item" :id="'question-'+ questionItem.itemOrder">
+                </el-form-item>
+<!--                选项-->
+                <el-form-item :key="Item.itemOrder" :label="Item.prefix+'、'+Item.content"
+                              v-for="Item in questionItem.items"
+                              class="exam-question-item" label-width="auto" label-position="left" :id="'question-'+ questionItem.itemOrder">
+                </el-form-item>
+                <!--                回答框-->
+                <el-form-item label="答案：" label-position="left" label-width="auto" >
+                  <el-input class="question-item-content-input"/>
+                </el-form-item>
               </el-form-item>
             </el-card>
           </el-row>
@@ -55,21 +66,22 @@ export default {
   },
 
   created () {
-    // 不知道后端接口于是写了组本地数据
+    // 不知道后端的接口于是写了组本地数据，到时根据后端的响应来改
     const response={"id":126,"level":1,"subjectId":4,"paperType":1,"name":"随机生成试卷", "titleItems":[{"name":"很随便的卷子",
-        "questionItems":[{"id":86,"questionType":1,"subjectId":4,"title":"第一题", "gradeLevel":1,
+        "questionItems":[{"id":86,"questionType":1,"subjectId":4,"title":"我是第一题，我的问题是blablabla", "gradeLevel":1,
           "items":[{"prefix":"A","content":"这是A","score":null},
             {"prefix":"B","content":"这是B","score":null},{"prefix":"C","content":"这是C","score":null},
             {"prefix":"D","content":"这是D","score":null}],"analyze":null,"correctArray":null,"correct":null,
-          "score":"2","difficult":null,"itemOrder":1,"knowledgeIdList":[]}]}],"score":"2","classes":null}
-    let id = this.$route.query.id
+          "score":"2","difficult":null,"itemOrder":1,"knowledgeIdList":[]}]}],"classes":null}
+    // let id = this.$route.query.id
     let _this = this
     _this.form = response
 
+    // 接受后端的数据，并把内容存入form中，API根据后端写的改
     // if (id && parseInt(id) !== 0) {
     //   _this.formLoading = true
     //   examPaperApi.select(id).then(re => {
-    //
+    //     _this.form=re
     //     _this.formLoading = false
     //   })
     // }
@@ -79,12 +91,7 @@ export default {
   beforeDestroy () {
   },
   methods: {
-    questionCompleted (completed) {
-      return this.enumFormat(this.doCompletedTag, completed)
-    },
-    goAnchor (selector) {
-      this.$el.querySelector(selector).scrollIntoView({ behavior: 'instant', block: 'center', inline: 'nearest' })
-    },
+    // 一个假的submit，不过它确实可以跳转页面
     submitForm () {
       let _this = this
       _this.formLoading = true

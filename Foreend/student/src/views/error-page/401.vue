f4c38f4dd5e77da545c4cc4c75bff05730bd173f
ryanregal <1822327087@qq.com>
2022-07-16 21:24:54 +0800

    试卷生成部分框架

--- Foreend/student/src/views/error-page/401.vue
+++ Foreend/student/src/views/error-page/401.vue
@@ -18,10 +18,6 @@
               回首页
             </router-link>
           </li>
-          <li class="link-type">
-            <a href="https://www.taobao.com/">随便看看</a>
-          </li>
-          <li><a href="#" @click.prevent="dialogVisible=true">点我看图</a></li>
         </ul>
       </el-col>
       <el-col :span="12">
--- Foreend/student/src/views/exam/paper/do.vue
+++ Foreend/student/src/views/exam/paper/do.vue
@@ -1,34 +1,13 @@
 <template>
   <div>
-    <el-row  class="do-exam-title">
-      <el-col :span="24">
-        <span :key="item.itemOrder"  v-for="item in answer.answerItems">
-             <el-tag :type="questionCompleted(item.completed)" class="do-exam-title-tag" @click="goAnchor('#question-'+item.itemOrder)">{{item.itemOrder}}</el-tag>
-        </span>
-        <span class="do-exam-time">
-          <label>剩余时间：</label>
-          <label>{{formatSeconds(remainTime)}}</label>
-        </span>
-      </el-col>
-    </el-row>
-    <el-row  class="do-exam-title-hidden">
-      <el-col :span="24">
-        <span :key="item.itemOrder"  v-for="item in answer.answerItems">
-             <el-tag  class="do-exam-title-tag" >{{item.itemOrder}}</el-tag>
-        </span>
-        <span class="do-exam-time">
-          <label>剩余时间：</label>
-        </span>
-      </el-col>
-    </el-row>
+
+<!--    试卷名字-->
     <el-container  class="app-item-contain">
       <el-header class="align-center">
         <h1>{{form.name}}</h1>
-        <div>
-          <span class="question-title-padding">试卷总分：{{form.score}}</span>
-          <span class="question-title-padding">考试时间：{{form.suggestTime}}分钟</span>
-        </div>
       </el-header>
+
+<!--      考试部分-->
       <el-main>
         <el-form :model="form" ref="form" v-loading="formLoading" label-width="100px">
           <el-row :key="index"  v-for="(titleItem,index) in form.titleItems">
@@ -42,6 +21,7 @@
               </el-form-item>
             </el-card>
           </el-row>
+<!--          提交部分-->
            <el-row class="do-align-center">
              <el-button type="primary" @click="submitForm">提交</el-button>
              <el-button>取消</el-button>
@@ -58,6 +38,8 @@ import { formatSeconds } from '@/utils'
 import QuestionEdit from '../components/QuestionEdit'
 import examPaperApi from '@/api/examPaper'
 import examPaperAnswerApi from '@/api/examPaperAnswer'
+/* eslint-disable */
+
 
 export default {
   components: { QuestionEdit },
@@ -67,84 +49,52 @@ export default {
       formLoading: false,
       answer: {
         questionId: null,
-        doTime: 0,
         answerItems: []
-      },
-      timer: null,
-      remainTime: 0
+      }
     }
   },
+
   created () {
+    // 不知道后端接口于是写了组本地数据
+    const response={"id":126,"level":1,"subjectId":4,"paperType":1,"name":"随机生成试卷", "titleItems":[{"name":"很随便的卷子",
+        "questionItems":[{"id":86,"questionType":1,"subjectId":4,"title":"第一题", "gradeLevel":1,
+          "items":[{"prefix":"A","content":"这是A","score":null},
+            {"prefix":"B","content":"这是B","score":null},{"prefix":"C","content":"这是C","score":null},
+            {"prefix":"D","content":"这是D","score":null}],"analyze":null,"correctArray":null,"correct":null,
+          "score":"2","difficult":null,"itemOrder":1,"knowledgeIdList":[]}]}],"score":"2","classes":null}
     let id = this.$route.query.id
     let _this = this
-    if (id && parseInt(id) !== 0) {
-      _this.formLoading = true
-      examPaperApi.select(id).then(re => {
-        _this.form = re.response
-        _this.remainTime = re.response.suggestTime * 60
-        _this.initAnswer()
-        _this.timeReduce()
-        _this.formLoading = false
-      })
-    }
+    _this.form = response
+
+    // if (id && parseInt(id) !== 0) {
+    //   _this.formLoading = true
+    //   examPaperApi.select(id).then(re => {
+    //
+    //     _this.formLoading = false
+    //   })
+    // }
   },
   mounted () {
-
   },
   beforeDestroy () {
-    window.clearInterval(this.timer)
   },
   methods: {
-    formatSeconds (theTime) {
-      return formatSeconds(theTime)
-    },
-    timeReduce () {
-      let _this = this
-      this.timer = setInterval(function () {
-        if (_this.remainTime <= 0) {
-          _this.submitForm()
-        } else {
-          ++_this.answer.doTime
-          --_this.remainTime
-        }
-      }, 1000)
-    },
     questionCompleted (completed) {
       return this.enumFormat(this.doCompletedTag, completed)
     },
     goAnchor (selector) {
       this.$el.querySelector(selector).scrollIntoView({ behavior: 'instant', block: 'center', inline: 'nearest' })
     },
-    initAnswer () {
-      this.answer.id = this.form.id
-      let titleItemArray = this.form.titleItems
-      for (let tIndex in titleItemArray) {
-        let questionArray = titleItemArray[tIndex].questionItems
-        for (let qIndex in questionArray) {
-          let question = questionArray[qIndex]
-          this.answer.answerItems.push({ questionId: question.id, content: null, contentArray: [], completed: false, itemOrder: question.itemOrder })
-        }
-      }
-    },
     submitForm () {
       let _this = this
-      window.clearInterval(_this.timer)
       _this.formLoading = true
-      examPaperAnswerApi.answerSubmit(this.answer).then(re => {
-        if (re.code === 200) {
-          _this.$alert('试卷得分：' + re.response + '分', '考试结果', {
-            confirmButtonText: '返回考试记录',
-            callback: action => {
-              _this.$router.push('/record/index')
-            }
-          })
-        } else {
-          _this.$message.error(re.message)
+      _this.$alert('成功提交！', {
+        confirmButtonText: '返回首页',
+        callback: action => {
+          _this.$router.push('/user/index')
         }
-        _this.formLoading = false
-      }).catch(e => {
-        _this.formLoading = false
       })
+      _this.formLoading = false
     }
   },
   computed: {

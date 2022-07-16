bf8a41953b10d3aa101af62966f05ce7d970e931
ryanregal <1822327087@qq.com>
2022-07-16 12:15:38 +0800

    用户select和delete修改

--- Foreend/admin/src/api/user.js
+++ Foreend/admin/src/api/user.js
@@ -1,12 +1,15 @@
 import { post } from '@/utils/request'
 
 export default {
-  // getUserEventPageList: query => post('/api/admin/user/event/page/list', query),
   getUserPageList: query => post('/api/admin/user/page/list', query), // 获得用户list
   createUser: query => post('/api/admin/user/edit', query), // 创建用户
-  selectUser: id => post('/api/admin/user/select/' + id), // 选择用户
   getCurrentUser: () => post('/api/admin/user/current'), // 获得现有用户
   updateUser: query => post('/api/admin/user/update', query), // 更新用户
-  deleteUser: id => post('/api/admin/user/delete/' + id), // 删除用户
+  selectUser: query => post('/api/admin/user/select/' ,query), // 选择用户
+  deleteUser: query => post('/api/admin/user/delete/', query), // 删除用户
+
+  //selectUser: id => post('/api/admin/user/select/' + id), // 选择用户
+  //deleteUser: id => post('/api/admin/user/delete/' + id), // 删除用户
   //selectByusername: query => post('/api/admin/user/selectByusername', query)
+  // getUserEventPageList: query => post('/api/admin/user/event/page/list', query),
 }

0646b98e9ec88fb36137a989f1bc96207db1f382
ZhichengLin <1317864235@qq.com>
2022-07-16 20:42:02 +0800

    选项

---
+++ Backend/src/main/java/edu/nine14/exam/entity/Option.java
@@ -0,0 +1,25 @@
+package edu.nine14.exam.entity;
+
+/**
+ * 选项
+ */
+public class Option{
+    private String prefix;
+    private String content;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}

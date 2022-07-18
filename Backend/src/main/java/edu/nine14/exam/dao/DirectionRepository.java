c68f3616e63be1c9448c09de0d5efbf6dc5207d9
ZhichengLin <1317864235@qq.com>
2022-07-18 17:02:01 +0800

    修改使用professionId查询direction返回内容

--- Backend/src/main/java/edu/nine14/exam/dao/DirectionRepository.java
+++ Backend/src/main/java/edu/nine14/exam/dao/DirectionRepository.java
@@ -2,10 +2,14 @@ package edu.nine14.exam.dao;
 
 import edu.nine14.exam.entity.Direction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
 
 @Repository
-public interface DirectionRepository extends CrudRepository<Direction, Integer> {
-
+public interface DirectionRepository extends JpaRepository<Direction, Integer> {
+    @Query(value = "select d from Direction d where d.profession.professionID=?1")
+    List<Direction> selectDirectionByProfessionId(Integer professionId);
 }

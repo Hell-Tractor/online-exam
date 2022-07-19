9c8a3804a4e2bb77604aafedbb8c098fb492ba44
吴佳熙 <1006961384@qq.com>
2022-07-19 09:52:03 +0800

    更新查询办法和返回值

--- Backend/src/main/java/edu/nine14/exam/dao/QuestionDirectionRepository.java
+++ Backend/src/main/java/edu/nine14/exam/dao/QuestionDirectionRepository.java
@@ -5,16 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface QuestionDirectionRepository extends JpaRepository<QuestionDirection, Integer>,
         JpaSpecificationExecutor<QuestionDirection> {
-    @Query("select q.questionID,q.direction,q.body,q.answer,q.type,q.selection from QuestionDirection q where q.direction.profession.professionID=?1 and q.type=?2")
-    List<Object> questionForProfession(Integer directionID,String type);
+    @Query(value="select question_bank.id,question_bank.body,question_bank.answer,question_bank.type,question_bank.selection from question_bank,direction,profession where question_bank.direction_id=direction.id and direction.profession_id=profession.id and profession.id=?1 and question_bank.type=?2",nativeQuery = true)
+    //@Query(value="select q.questionID,q.body,q.answer,q.type,q.selection from QuestionDirection q where q.direction.profession.professionID=?1 and q.type=?2")
+    List<Map<String,Object>> questionForProfession(Integer directionID, String type);
 
-    @Query("select q.questionID,q.direction,q.body,q.answer,q.type,q.selection from QuestionDirection q where q.direction.profession.professionID=?1 and q.type=?2 and q.direction.directionName IN (?3,?4)")
-    List<Object> questionForDirection(Integer directionID,String type,String direction1,String direction2);
+    @Query(value="select question_bank.id,question_bank.body,question_bank.answer,question_bank.type,question_bank.selection from question_bank,direction,profession where question_bank.direction_id=direction.id and direction.profession_id=profession.id and profession.id=?1 and question_bank.type=?2 and direction.name IN (?3,?4)",nativeQuery = true)
+    //@Query(value="select q.questionID,q.body,q.answer,q.type,q.selection from QuestionDirection q where q.direction.profession.professionID=?1 and q.type=?2 and q.direction.directionName IN (?3,?4)")
+    List<Map<String,Object>> questionForDirection(Integer directionID,String type,String direction1,String direction2);
 }

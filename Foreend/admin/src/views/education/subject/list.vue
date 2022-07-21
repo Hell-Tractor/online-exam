b590e6ec6c6e71f2102d4e30a290f7a924bd91c6
ryanregal <1822327087@qq.com>
2022-07-21 20:54:58 +0800

    页面的调整

--- Foreend/admin/src/views/education/subject/list.vue
+++ Foreend/admin/src/views/education/subject/list.vue
@@ -26,8 +26,8 @@
         </template>
       </el-table-column>
     </el-table>
-    <pagination v-show="total>0" :total="total" :page.sync="queryParam.pageIndex" :limit.sync="queryParam.pageSize"
-                @pagination="search"/>
+<!--    <pagination v-show="total>0" :total="total" :page.sync="queryParam.pageIndex" :limit.sync="queryParam.pageSize"-->
+<!--                @pagination="search"/>-->
   </div>
 </template>
 
